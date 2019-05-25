1e9d3e63280aa1ba428faea5e3ed01d8cc52ba81
Luiz Américo <luizamericop@gmail.com>
2019-05-25 20:55:58 -0300

    Add tests for subsetting woff/woff2 files

--- test/subset.js
+++ test/subset.js
@@ -120,4 +120,106 @@ describe('font subsetting', function() {
       }));
     });
   });
+
+  describe('WOFF ttf subsetting', function() {
+    let font = fontkit.openSync(__dirname + '/data/SourceSansPro/SourceSansPro-Regular.ttf.woff');
+
+    it('should create a TTFSubset instance', function() {
+      let subset = font.createSubset();
+      return assert.equal(subset.constructor.name, 'TTFSubset');
+    });
+
+    it('should produce a subset', function(done) {
+      let subset = font.createSubset();
+      let iterable = font.glyphsForString('hello');
+      for (let i = 0; i < iterable.length; i++) {
+        let glyph = iterable[i];
+        subset.includeGlyph(glyph);
+      }
+
+      return subset.encodeStream().pipe(concat(function(buf) {
+        let f = fontkit.create(buf);
+        assert.equal(f.numGlyphs, 5);
+        assert.equal(f.getGlyph(1).path.toSVG(), font.glyphsForString('h')[0].path.toSVG());
+        done();
+      }));
+    });
+  });
+
+  describe('WOFF otf subsetting', function() {
+    let font = fontkit.openSync(__dirname + '/data/SourceSansPro/SourceSansPro-Regular.otf.woff');
+
+    it('should create a CFFSubset instance', function() {
+      let subset = font.createSubset();
+      return assert.equal(subset.constructor.name, 'CFFSubset');
+    });
+
+    it('should produce a subset', function(done) {
+      let subset = font.createSubset();
+      let iterable = font.glyphsForString('hello');
+      for (let i = 0; i < iterable.length; i++) {
+        let glyph = iterable[i];
+        subset.includeGlyph(glyph);
+      }
+
+      return subset.encodeStream().pipe(concat(function(buf) {
+        let stream = new r.DecodeStream(buf);
+        let cff = new CFFFont(stream);
+        let glyph = new CFFGlyph(1, [], { stream, 'CFF ': cff });
+        assert.equal(glyph.path.toSVG(), font.glyphsForString('h')[0].path.toSVG());
+        return done();
+      }));
+    });
+  });
+
+  describe.skip('WOFF2 ttf subsetting', function() {
+    let font = fontkit.openSync(__dirname + '/data/SourceSansPro/SourceSansPro-Regular.ttf.woff2');
+
+    it('should create a TTFSubset instance', function() {
+      let subset = font.createSubset();
+      return assert.equal(subset.constructor.name, 'TTFSubset');
+    });
+
+    it('should produce a subset', function(done) {
+      let subset = font.createSubset();
+      let iterable = font.glyphsForString('hello');
+      for (let i = 0; i < iterable.length; i++) {
+        let glyph = iterable[i];
+        subset.includeGlyph(glyph);
+      }
+
+      return subset.encodeStream().pipe(concat(function(buf) {
+        let f = fontkit.create(buf);
+        assert.equal(f.numGlyphs, 5);
+        assert.equal(f.getGlyph(1).path.toSVG(), font.glyphsForString('h')[0].path.toSVG());
+        done();
+      }));
+    });
+  });
+
+  describe('WOFF2 otf subsetting', function() {
+    let font = fontkit.openSync(__dirname + '/data/SourceSansPro/SourceSansPro-Regular.otf.woff2');
+
+    it('should create a CFFSubset instance', function() {
+      let subset = font.createSubset();
+      return assert.equal(subset.constructor.name, 'CFFSubset');
+    });
+
+    it('should produce a subset', function(done) {
+      let subset = font.createSubset();
+      let iterable = font.glyphsForString('hello');
+      for (let i = 0; i < iterable.length; i++) {
+        let glyph = iterable[i];
+        subset.includeGlyph(glyph);
+      }
+
+      return subset.encodeStream().pipe(concat(function(buf) {
+        let stream = new r.DecodeStream(buf);
+        let cff = new CFFFont(stream);
+        let glyph = new CFFGlyph(1, [], { stream, 'CFF ': cff });
+        assert.equal(glyph.path.toSVG(), font.glyphsForString('h')[0].path.toSVG());
+        return done();
+      }));
+    });
+  });   
 });
